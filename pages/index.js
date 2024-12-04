93eab2cb9389dfa0b2e2e3f15e6e7058de70b667
titrn <146559025+titrn@users.noreply.github.com>
2024-12-04 16:14:41 -0500

    manual set up Next.js frontend

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home(){
+    return (
+        <div>
+            <h1>Welcome to Titan's Local Business Discovery App</h1>
+            <>Check out these pop-ups! Support local!</>
+        </div>
+    );
+}
